dbc57576f8cae29341dd2d0cde197ddc2dccdc6f
Kien <membercode.ptk21@gmail.com>
2022-11-16 00:04:32 +0700

    fist demo

--- websocket_fundametal/src/main/java/com/example/websocket_fundametal/ws/MyHandler.java
+++ websocket_fundametal/src/main/java/com/example/websocket_fundametal/ws/MyHandler.java
@@ -2,17 +2,15 @@ package com.example.websocket_fundametal.ws;
 
 //import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 //@Slf4j
-public class DataHandler extends TextWebSocketHandler {
+public class MyHandler extends TextWebSocketHandler {
     @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 //        log.info("Message: {}", message.getPayload());
-        session.sendMessage(new TextMessage("Hello back"));
-        super.handleMessage(session, message);
-
+        //session.sendMessage(new TextMessage("OK! Hello back"));
+        //super.handleMessage(session, message);
     }
 }
